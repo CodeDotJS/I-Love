d189433d62526cc3e5717cda22501176bef57215
Rishi Giri <rushforlinux@gmail.com>
2016-02-08 07:31:16 +0530

    unfinished

--- cli.js
+++ cli.js
@@ -3,48 +3,30 @@ const colors = require('colors');
 const argv = require('yargs')
     .usage('Usage: $0 -u [/user.name]'.info)
     .demand(['u'])
+    .describe('u', ' : black | red | green | yellow | blue | magenta | cyan')
     .argv;
 
 colors.setTheme({
-  info: 'rainbow'
+	info: [argv.u, 'bold']
 });
 
-console.log("\n\t 	❦❦❦❦❦❦           ❦❦❦❦❦❦ 				".info);
-
-console.log("\t      ❦❦❦❦❦❦❦❦❦❦       ❦❦❦❦❦❦❦❦❦❦ 			".info);
-
-console.log("\t    ❦❦❦❦❦❦❦❦❦❦❦❦❦❦   ❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 			".info);
-
-console.log("\t  ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
-
-console.log("\t ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
-
-console.log("\t❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
-
-console.log("\t❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
-
-console.log("\t❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
-
-console.log("\t ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
-
-console.log("\t  ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
-
-console.log("\t   ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
-
-console.log("\t    ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 			".info);
-
-console.log("\t      ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 			".info);
-
-console.log("\t        ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 				".info);
-
-console.log("\t          ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 				".info);
-
-console.log("\t            ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 					".info);
-
-console.log("\t              ❦❦❦❦❦❦❦❦❦❦❦ 					".info);
-
-console.log("\t                ❦❦❦❦❦❦❦  					".info);
-
-console.log("\t                  ❦❦❦ 						".info);
-
-console.log("\t                   ❦\n 						".info);
+console.log('\n\t      	❦❦❦❦❦❦           ❦❦❦❦❦❦ 										'.info);
+console.log('\t       ❦❦❦❦❦❦❦❦❦❦       ❦❦❦❦❦❦❦❦❦❦ 								'.info);
+console.log('\t     ❦❦❦❦❦❦❦❦❦❦❦❦❦❦   ❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 						'.info);
+console.log('\t   ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 				'.info);
+console.log('\t  ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 			'.info);
+console.log('\t ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		'.info);
+console.log('\t ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		'.info);
+console.log('\t ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		'.info);
+console.log('\t  ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 			'.info);
+console.log('\t   ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 				'.info);
+console.log('\t    ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 					'.info);
+console.log('\t     ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 						'.info);
+console.log('\t       ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 								'.info);
+console.log('\t         ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 										'.info);
+console.log('\t           ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 												'.info);
+console.log('\t             ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 														'.info);
+console.log('\t               ❦❦❦❦❦❦❦❦❦❦❦ 																'.info);
+console.log('\t                 ❦❦❦❦❦❦❦  																	'.info);
+console.log('\t                   ❦❦❦ 																				'.info);
+console.log('\t                    ❦\n 																			'.info);
