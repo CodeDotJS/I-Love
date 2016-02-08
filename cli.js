8fb6322ac13f0cc2a595463c27884309cce0985b
Rishi Giri <rushforlinux@gmail.com>
2016-02-08 05:30:54 +0530

    unfinished

---
+++ cli.js
@@ -0,0 +1,50 @@
+const colors = require('colors');
+
+const argv = require('yargs')
+    .usage('Usage: $0 -u [/user.name]'.info)
+    .demand(['u'])
+    .argv;
+
+colors.setTheme({
+  info: 'rainbow'
+});
+
+console.log("\n\t 	❦❦❦❦❦❦           ❦❦❦❦❦❦ 				".info);
+
+console.log("\t      ❦❦❦❦❦❦❦❦❦❦       ❦❦❦❦❦❦❦❦❦❦ 			".info);
+
+console.log("\t    ❦❦❦❦❦❦❦❦❦❦❦❦❦❦   ❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 			".info);
+
+console.log("\t  ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
+
+console.log("\t ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
+
+console.log("\t❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
+
+console.log("\t❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
+
+console.log("\t❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
+
+console.log("\t ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
+
+console.log("\t  ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
+
+console.log("\t   ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 		".info);
+
+console.log("\t    ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 			".info);
+
+console.log("\t      ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 			".info);
+
+console.log("\t        ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 				".info);
+
+console.log("\t          ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 				".info);
+
+console.log("\t            ❦❦❦❦❦❦❦❦❦❦❦❦❦❦❦ 					".info);
+
+console.log("\t              ❦❦❦❦❦❦❦❦❦❦❦ 					".info);
+
+console.log("\t                ❦❦❦❦❦❦❦  					".info);
+
+console.log("\t                  ❦❦❦ 						".info);
+
+console.log("\t                   ❦\n 						".info);
